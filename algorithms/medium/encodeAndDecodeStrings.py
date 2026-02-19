4cd23a84b4fdd43ecfe78ce3dcfe56af4a77f42b
Will's pc <billxu321@gmail.com>
2026-02-18 22:07:06 -0600

    271

---
+++ algorithms/medium/encodeAndDecodeStrings.py
@@ -0,0 +1,28 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            l = int(s[i:j])
+            res.append(s[j+1:j+1+l])
+            i = j + 1 + l
+        return res
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
